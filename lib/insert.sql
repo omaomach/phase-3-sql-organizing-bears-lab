bb29eb491ea273e5054af464a81d98dcc0ede58e
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-10-29 08:34:36 +0300

    phase 3 sql organizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Brown", "High", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "F", "Black", "Low", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "Brown", "Medium", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 15, "M", "Brown", "High", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "White", "Low", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "Black", "High", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, "F", "Brown", "Low", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, "M", "Brown", "High", 1);
+
+
+
+
+
+
+
